d3cfa5f5490d681268537ed769c0a47a3a9aec6e
StarHunter729 <69334359+StarHunter729@users.noreply.github.com>
2020-11-14 23:11:54 -0600

    Update index.php

--- public_html/admin/index.php
+++ public_html/admin/index.php
@@ -1,1 +1,26 @@
-<?php echo "Hello World" ?>
+<html>
+    <head>
+        <Title>Admin Login</Title>
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container"> 
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Login</h2>
+                    <form action="login.php"  method="post">
+                        <div class="form-group">
+                            <label>Email</label> 
+                            <input type="text" name="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Contraseña</label>
+                            <input type="password" name="pass" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
